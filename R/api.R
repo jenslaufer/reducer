f79ed56993b5e98fba7dc0280cb050426ef58cfd
jenslaufer <jenslaufer@gmail.com>
2019-10-07 09:22:18 +0200

    implemented reduce function

--- R/api.R
+++ R/api.R
@@ -1,6 +1,28 @@
-library(tidyverse)
+.get.super.unit <- function(unit) {
+  switch(
+    unit,
+    "second" = "minute",
+    "minute" = "hour",
+    "hour" = "day",
+    "day" = "month",
+    "month" = "year"
+  )
+}
 
-
-reduce <- function(data, persiod, unit, date_field){
+reduce <- function(df, time.interval, unit, date_field) {
+  super.unit <- .get.super.unit(unit)
+  unit.pl <- "lubridate::{unit}s" %>% glue::glue()
+  unit <- "lubridate::{unit}" %>% glue::glue()
   
+  df %>%
+    dplyr::mutate(
+      interval = lubridate::floor_date(!!as.name(date_field), unit = super.unit) + eval(parse(text = unit.pl))(ceiling(
+        eval(parse(text = unit))(!!as.name(date_field)) / time.interval
+      ) * time.interval)
+    ) %>%
+    dplyr::group_by(interval) %>%
+    dplyr::summarise_all(~ mean(.)) %>%
+    dplyr::ungroup() %>%
+    dplyr::select(-date) %>%
+    dplyr::rename(date = interval)
 }
